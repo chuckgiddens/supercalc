4f320d9a1a2a66d882d6da78f2d293baae0af6a0
Chuck Giddens <chuck.c.giddens@ey.com>
2021-02-11 12:56:01 -0500

    Project Generation Completed

---
+++ SuperCalc/SuperCalc/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SuperCalc.Services;
+using SuperCalc.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SuperCalc
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SuperCalc/SuperCalc/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SuperCalc.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SuperCalc.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
